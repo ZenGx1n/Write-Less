20742eb66014af53b480a61da43b83ecd152b507
ZenGx1n <1534408497@qq.com>
2019-08-09 19:54:50 +0800

    static

---
+++ wl-dao/src/main/java/com/writeless/global/Contant.java
@@ -0,0 +1,16 @@
+package com.writeless.global;
+
+public class Contant {
+
+    /**
+     * 职业
+     */
+    public static final String POST_STAFF = "员工";
+    public static final String POST_FM = "部门经理";
+    public static final String POST_GM = "总经理";
+    public static final String POST_CASHIER = "财务";
+    public static final String POST_SALE = "销售";
+    public static final String POST_PROCUREMENT = "采购";
+    public static final String POST_PERSONNEL = "人事";
+
+}
